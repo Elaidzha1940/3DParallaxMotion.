478eacf81e2956c213a204ec0fabc08a6372bbcf
Elaidzha1940 <eli080293@gmail.com>
2024-04-21 16:50:25 +0300

    3DParallaxMotion

---
+++ 3DParallaxMotion/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  3DParallaxMotion
+//
+//  Created by Elaidzha Shchukin on 21.04.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+        
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ 3DParallaxMotion/_DParallaxMotionApp.swift
@@ -0,0 +1,17 @@
+//
+//  _DParallaxMotionApp.swift
+//  3DParallaxMotion
+//
+//  Created by Elaidzha Shchukin on 21.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct _DParallaxMotionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
